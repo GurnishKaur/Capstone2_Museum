f10804bc41c0e63ed8eeedee399d39c347ee05f9
Gurnish Kaur <123663851+GurnishKaur@users.noreply.github.com>
2024-07-04 12:09:36 -0400

    Add files via upload

---
+++ events-script.js
@@ -0,0 +1,7 @@
+// scripts.js
+
+// Function to toggle the chatbot widget
+function toggleChatbot() {
+    const chatbotWidget = document.getElementById('chatbot-widget');
+    chatbotWidget.classList.toggle('minimized');
+}
